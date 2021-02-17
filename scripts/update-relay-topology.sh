4951841b90e7d720e25de38affd16afe1e93121e
John Ky <john.ky@iohk.io>
2021-02-17 21:46:48 +1100

    Stuff

--- scripts/update-relay-topology.sh
+++ scripts/update-relay-topology.sh
@@ -33,4 +33,4 @@ if [ ! -d /opt/cardano-node/logs ]; then
   mkdir -p /opt/cardano-node/logs;
 fi
  
-curl -s "https://api.clio.one/htopology/v1/?port=${CNODE_PORT}&blockNo=${blockNo}&valency=${CNODE_VALENCY}&magic=${NWMAGIC}${T_HOSTNAME}" | tee -a /opt/cardano-node/logs/topologyUpdater_lastresult.json
+curl -s "https://api.clio.one/htopology/v1/?port=${CNODE_PORT}&blockNo=${blockNo}&valency=${CNODE_VALENCY}&magic=${NWMAGIC}${T_HOSTNAME}" | tee -a /opt/cardano-node/logs/update-relay-topology--last-result.json
