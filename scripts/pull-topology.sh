c341f7d2a45e519b3e944d81f7abb062398e1e3b
John Ky <john.ky@iohk.io>
2021-02-18 00:23:50 +1100

    Increase number of peer s

--- scripts/pull-topology.sh
+++ scripts/pull-topology.sh
@@ -3,9 +3,9 @@
 source /opt/cardano-node/machine.env
 
 curl -s -f \
-  "https://api.clio.one/htopology/v1/fetch/?max=1&magic=764824073&customPeers=core.${WORKSPACE}.tokendynamics.net:3001" \
+  "https://api.clio.one/htopology/v1/fetch/?max=10&magic=764824073&customPeers=core.${WORKSPACE}.tokendynamics.net:3001" \
   | jq . > "/opt/cardano-node/config/relay-topology.json"
 
 curl -s -f \
-  "https://api.clio.one/htopology/v1/fetch/?max=1&magic=764824073&customPeers=relay.${WORKSPACE}.tokendynamics.net:3001" \
+  "https://api.clio.one/htopology/v1/fetch/?max=10&magic=764824073&customPeers=relay.${WORKSPACE}.tokendynamics.net:3001" \
   | jq . > "/opt/cardano-node/config/core-topology.json"
