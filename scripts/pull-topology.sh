e69e09f50604ca827fb693c5090fb13cc15764ff
John Ky <john.ky@iohk.io>
2021-02-18 00:19:59 +1100

    New pull-topology.sh script

---
+++ scripts/pull-topology.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /opt/cardano-node/machine.env
+
+curl -s -f \
+  "https://api.clio.one/htopology/v1/fetch/?max=1&magic=764824073&customPeers=core.${WORKSPACE}.tokendynamics.net:3001" \
+  | jq . > "/opt/cardano-node/config/relay-topology.json"
+
+curl -s -f \
+  "https://api.clio.one/htopology/v1/fetch/?max=1&magic=764824073&customPeers=relay.${WORKSPACE}.tokendynamics.net:3001" \
+  | jq . > "/opt/cardano-node/config/core-topology.json"
