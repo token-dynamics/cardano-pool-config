4e13adef6bd5965ae9b7439f3d1e477ed5259aaf
John Ky <john.ky@iohk.io>
2021-02-22 16:17:55 +1100

    Bump up to 20 peers

--- scripts/pull-topology.sh
+++ scripts/pull-topology.sh
@@ -3,7 +3,7 @@
 source /opt/cardano-node/machine.env
 
 curl -s -f \
-  "https://api.clio.one/htopology/v1/fetch/?max=10&magic=764824073&customPeers=core.${WORKSPACE}.tokendynamics.net:3001" \
+  "https://api.clio.one/htopology/v1/fetch/?max=20&magic=764824073&customPeers=core.${WORKSPACE}.tokendynamics.net:3001" \
   | jq . > "/opt/cardano-node/config/relay-topology.json"
 
 cat > "/opt/cardano-node/config/core-topology.json" <<EOF
