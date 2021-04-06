9117e69914c436d76826022c7063e160e5ed6f53
John Ky <john.ky@iohk.io>
2021-04-06 15:14:18 +1000

    More relays

--- scripts/pull-topology.sh
+++ scripts/pull-topology.sh
@@ -2,15 +2,43 @@
 
 source /opt/cardano-node/machine.env
 
+mkdir -p /opt/cardano-node/config/backup
+
+if [ -f /opt/cardano-node/config/relay-topology.json ]; then
+  backup="$(mktemp /opt/cardano-node/config/backup/relay-topology.json.XXXXXX)"
+  echo "Backing up to $backup"
+  cp /opt/cardano-node/config/relay-topology.json "$backup"
+fi
+
+if [ -f /opt/cardano-node/config/core-topology.json ]; then
+  backup="$(mktemp /opt/cardano-node/config/backup/core-topology.json.XXXXXX)"
+  echo "Backing up to $backup"
+  cp /opt/cardano-node/config/core-topology.json "$backup"
+fi
+
+echo "Generating /opt/cardano-node/config/relay-topology.json"
+
 curl -s -f \
   "https://api.clio.one/htopology/v1/fetch/?max=20&magic=764824073&customPeers=core.${WORKSPACE}.tokendynamics.net:3001" \
   | jq . > "/opt/cardano-node/config/relay-topology.json"
+
+echo "Generating /opt/cardano-node/config/core-topology.json"
 
 cat > "/opt/cardano-node/config/core-topology.json" <<EOF
 {
   "Producers": [
     {
       "addr": "relay.${WORKSPACE}.tokendynamics.net",
+      "port": 3001,
+      "valency": 1
+    },
+    {
+      "addr": "relay-1.${WORKSPACE}.tokendynamics.net",
+      "port": 3001,
+      "valency": 1
+    },
+    {
+      "addr": "relay-2.${WORKSPACE}.tokendynamics.net",
       "port": 3001,
       "valency": 1
     }
