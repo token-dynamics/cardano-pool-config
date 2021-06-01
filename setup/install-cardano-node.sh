74695f4db8dcf2b1d4f4991af2dc06d4492680c6
John Ky <john.ky@iohk.io>
2021-06-01 11:00:56 +1000

    Upgrade to cardano-node-1.27.0

--- setup/install-cardano-node.sh
+++ setup/install-cardano-node.sh
@@ -4,7 +4,7 @@ tmpdir=$(mktemp -d)
 trap 'rm -rf "$tmpdir"' EXIT
 
 curl https://dl.haskellworks.io/binaries/libsodium/23.3.0/libsodium.tar.gz | tar zxvf - -C /usr
-curl https://dl.haskellworks.io/binaries/cardano-node/1.26.2/cardano-node-x86_64_linux.tar.gz | tar zxvf - -C /usr/local/bin
-curl https://dl.haskellworks.io/binaries/cardano-cli/1.26.2/cardano-cli-x86_64_linux.tar.gz | tar zxvf - -C /usr/local/bin
+curl https://dl.haskellworks.io/binaries/cardano-node/1.27.0/cardano-node-x86_64_linux.tar.gz | tar zxvf - -C /usr/local/bin
+curl https://dl.haskellworks.io/binaries/cardano-cli/1.27.0/cardano-cli-x86_64_linux.tar.gz | tar zxvf - -C /usr/local/bin
 
 cardano-node version
