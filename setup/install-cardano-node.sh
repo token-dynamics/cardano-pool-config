10f6f21e3161044851af3f7d37cf9bb011970d9e
John Ky <john.ky@iohk.io>
2021-10-15 19:09:19 +1100

    Download from alternate location

--- setup/install-cardano-node.sh
+++ setup/install-cardano-node.sh
@@ -7,7 +7,7 @@ curl https://dl.haskellworks.io/binaries/libsodium/23.3.0/libsodium.tar.gz | tar
 
 mkdir /usr/local/cardano-node-1.31.1
 
-curl https://dl.haskellworks.io/binaries/cardano-node/cardano-node-1.31.1-linux.tar.gz | tar zxvf - -C /usr/local/cardano-node-1.31.1
+curl https://hydra.iohk.io/build/7739415/download/1/cardano-node-1.30.1-linux.tar.gz | tar zxvf - -C /usr/local/cardano-node-1.31.1
 
 ln -sf /usr/local/cardano-node-1.31.1 /usr/local/cardano-node
 
