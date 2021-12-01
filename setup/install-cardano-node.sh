3b29388074c64e893a7f3d89b083e055d8876be1
John Ky <john.ky@iohk.io>
2021-12-01 22:57:44 +1100

    Upgrade to cardano-node-1.31.0

--- setup/install-cardano-node.sh
+++ setup/install-cardano-node.sh
@@ -3,15 +3,17 @@
 tmpdir=$(mktemp -d)
 trap 'rm -rf "$tmpdir"' EXIT
 
+node_version=1.31.0
+
 curl https://dl.haskellworks.io/binaries/libsodium/23.3.0/libsodium.tar.gz | tar zxvf - -C /usr
 
-mkdir -p /usr/local/cardano-node-1.30.1
+mkdir -p /usr/local/cardano-node-${node_version}
 
-curl https://hydra.iohk.io/build/7739415/download/1/cardano-node-1.30.1-linux.tar.gz | tar zxvf - -C /usr/local/cardano-node-1.30.1
+curl https://hydra.iohk.io/build/8110920/download/1/cardano-node-${node_version}-linux.tar.gz | tar zxvf - -C /usr/local/cardano-node-${node_version}
 
 rm -f /usr/local/cardano-node
 
-ln -sf /usr/local/cardano-node-1.30.1 /usr/local/cardano-node
+ln -sf /usr/local/cardano-node-${node_version} /usr/local/cardano-node
 
 ln -sf /usr/local/cardano-node/cardano-node /usr/local/bin/cardano-node
 ln -sf /usr/local/cardano-node/cardano-cli  /usr/local/bin/cardano-cli
